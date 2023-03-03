16b52de834f7cd76f23402ceaf3b1d45c306ecf9
Artem Bahmet <baget.masta@gmail.com>
2023-03-03 12:07:32 +0200

    removes unnecessary code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,19 @@
-import { Box } from '../utils/Box';
 import { Card } from './Card/Card';
 import { PeopleListContainer } from './Card/Card.styled';
 import users from '../users.json';
 
 export const App = () => {
   return (
-    <Box>
-      <PeopleListContainer>
-        {users.map(({ id, tweets, followers, avatar }) => (
-          <Card
-            key={id}
-            tweets={tweets}
-            followers={followers}
-            avatar={avatar}
-            id={id}
-          />
-        ))}
-      </PeopleListContainer>
-    </Box>
+    <PeopleListContainer>
+      {users.map(({ id, tweets, followers, avatar }) => (
+        <Card
+          key={id}
+          tweets={tweets}
+          followers={followers}
+          avatar={avatar}
+          id={id}
+        />
+      ))}
+    </PeopleListContainer>
   );
 };
